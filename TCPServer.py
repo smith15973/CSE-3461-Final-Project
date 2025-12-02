a9f1d5bb3919dd3e0ba7293e79f9b66a199df71e
Noah Smith <smith.15973@osu.edu>
2025-12-01 23:38:10 -0500

    gracefully shutdown server

--- TCPServer.py
+++ TCPServer.py
@@ -40,14 +40,18 @@ def main():
     print('The server is ready to receive')  # printing to confirm that TCP server is up and ready
 
     while True:   #always welcoming
-        connectionSocket, addr = serverSocket.accept()  #create connection socket when client requests
-        connectionSocket.send(f"Address: {addr[0]}, Port: {addr[1]}".encode()) #send client address and port info
-        
-        with clients_lock:
-            clients.append(connectionSocket) #add client socket to list of clients
+        try:
+            connectionSocket, addr = serverSocket.accept()  #create connection socket when client requests
+            connectionSocket.send(f"Address: {addr[0]}, Port: {addr[1]}".encode()) #send client address and port info
+            
+            with clients_lock:
+                clients.append(connectionSocket) #add client socket to list of clients
 
-        t = threading.Thread(target=handleClientWork, args=(connectionSocket,)) #create thread for client
-        t.start() # start client thread
+            t = threading.Thread(target=handleClientWork, args=(connectionSocket,)) #create thread for client
+            t.start() # start client thread
+        except KeyboardInterrupt:
+            print("\nShutting down Server")
+            break
 
 if __name__ == "__main__":
     main()
