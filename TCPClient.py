bb172ac01adcd661ecb9f4219ad007d849e890b4
Noah Smith <smith.15973@osu.edu>
2025-12-01 22:39:26 -0500

    basic broadcasting an receiving working for server and client. Need to implement thread locks. Need to fix server shutdown errors.

--- TCPClient.py
+++ TCPClient.py
@@ -1,13 +1,35 @@
 from socket import *
+from threading import Thread
+
 serverName = "127.0.0.1" #or local host "127.0.0.1"#"192.168.1.2"#'hostname'#server's IP address (precisely IPv4)'servername'
 serverPort = 12000 #un-reserved port #
-clientSocket = socket(AF_INET, SOCK_STREAM) #creates client side TCP socket
-clientSocket.connect((serverName,serverPort)) # initiates TCP connection . After this line is executed, three-way handshake is performed and a 
-# TCP connection is established
+connectionOpen = False
 
-sentence = input('Input lowercase sentence:') #reads the string from client side user
-clientSocket.send(sentence.encode()) # this line encodes and sends the string 
+def handleServerMessages(clientSocket: socket):
+    while True:   #always listening for messages
+        msg = clientSocket.recv(1024).decode() #receives 'string' from server, and decodes it first
 
-modifiedSentence = clientSocket.recv(1024) # the string is received here after getting modified from server. recv() method receives data from a socket and stores it in a buffer
-print('From Server: ', modifiedSentence.decode()) # the string (now in UPPERCASE) is received from server, and  decoded.
-clientSocket.close() # this closes the socket
+        print('From Server:', msg)
+
+        if not msg:
+            clientSocket.close()  #connection closes
+            print("Server shutdown")
+            global connectionOpen
+            connectionOpen = False
+            break
+
+def main():
+    clientSocket = socket(AF_INET, SOCK_STREAM) #creates client side TCP socket
+    clientSocket.connect((serverName,serverPort)) # initiates TCP connection . After this line is executed, three-way handshake is performed and a TCP connection is established
+    addressInfo = clientSocket.recv(1024).decode() #receives 'string' from server, and decodes it first
+    print(addressInfo)
+    t = Thread(target=handleServerMessages, args=(clientSocket,))
+    t.start()
+    global connectionOpen
+    connectionOpen = True
+    while connectionOpen:
+        sentence = input('Message:') #reads the string from client side user
+        clientSocket.send(sentence.encode()) # this line encodes and sends the string 
+
+if __name__ == "__main__":
+    main()
--- TCPServer.py
+++ TCPServer.py
@@ -1,16 +1,38 @@
 from socket import * 
+import threading
 serverPort = 12000 
-serverSocket = socket(AF_INET,SOCK_STREAM) #creating a server side socket
-serverSocket.bind(('',serverPort))  # bind() method associates a server socket with a specific address and port on the local machine
-serverSocket.listen(1) #this line means server listens for the TCP connection req. 
-print('The server is ready to receive')  # printing to confirm that TCP server is up and ready
-while True:   #always welcoming
-    connectionSocket, addr = serverSocket.accept()  #When a client knocks on this door, the program invokes the  method for serverSocket, 
-    #which creates a new socket in the server, called , dedicated to this particular client.
+clients: list[socket] = []
 
-    sentence = connectionSocket.recv(1024).decode() #receives 'string' from client, and decodes it first
+def broadcast(sender: socket, msg:str):
+    for client in clients:
+        if client is not sender:
+                client.send(msg.encode())
 
-    capitalizedSentence = sentence.upper()    #converts to an UPPERCASE
+def handleClientWork(connectionSocket: socket):
+    while True:   #always listening for messages
+        msg = connectionSocket.recv(1024).decode() #receives 'string' from client, and decodes it first
 
-    connectionSocket.send(capitalizedSentence.encode()) # sends back to the client
-    connectionSocket.close()  #connection closes
+        if not msg:
+            connectionSocket.close()  #connection closes
+            clients.remove(connectionSocket)
+            break
+
+        broadcast(connectionSocket, msg)
+
+def main():
+    serverSocket = socket(AF_INET,SOCK_STREAM) #creating a server side socket
+    serverSocket.bind(('',serverPort))  # bind() method associates a server socket with a specific address and port on the local machine
+    serverSocket.listen(1) #this line means server listens for the TCP connection req. 
+    print('The server is ready to receive')  # printing to confirm that TCP server is up and ready
+
+    while True:   #always welcoming
+        connectionSocket, addr = serverSocket.accept()  #create connection socket when client requests
+        connectionSocket.send(f"Address: {addr[0]}, Port: {addr[1]}".encode()) #send client address and port info
+        
+        clients.append(connectionSocket) #add client socket to list of clients
+
+        t = threading.Thread(target=handleClientWork, args=(connectionSocket,)) #create thread for client
+        t.start() # start client thread
+
+if __name__ == "__main__":
+    main()
