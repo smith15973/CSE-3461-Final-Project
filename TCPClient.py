628e6ccae2a9d62bd1e1d2ae38a29b9fd9c3dc3a
Noah Smith <smith.15973@osu.edu>
2025-12-01 23:32:14 -0500

    more graceful server disconnects. moved input to its own thread

--- TCPClient.py
+++ TCPClient.py
@@ -1,22 +1,46 @@
 from socket import *
 from threading import Thread
+import sys
 
 serverName = "127.0.0.1" #or local host "127.0.0.1"#"192.168.1.2"#'hostname'#server's IP address (precisely IPv4)'servername'
 serverPort = 12000 #un-reserved port #
 connectionOpen = False
 
 def handleServerMessages(clientSocket: socket):
+    global connectionOpen
     while True:   #always listening for messages
-        msg = clientSocket.recv(1024).decode() #receives 'string' from server, and decodes it first
+        try:
+            msg = clientSocket.recv(1024) #receives 'string' from server
 
-        print('From Server:', msg)
+            if not msg:
+                clientSocket.close()  #connection closes
+                print("\nServer disconnected")
+                connectionOpen = False
+                break
 
-        if not msg:
-            clientSocket.close()  #connection closes
-            print("Server shutdown")
-            global connectionOpen
+            print('From Server:', msg.decode())
+        except Exception as e:
+            print(f"Connection error: {e}")
             connectionOpen = False
             break
+
+    clientSocket.close()
+
+def handleUserInput(clientSocket: socket):
+    global connectionOpen
+    connectionOpen = True
+    try:
+        while connectionOpen:
+            sentence = input('Message:') #reads the string from client side user
+            if not connectionOpen:
+                break
+            clientSocket.send(sentence.encode()) # this line encodes and sends the string 
+    except KeyboardInterrupt:
+        print("\nClient shutting down...")
+    finally:
+        clientSocket.close()
+
+
 
 def main():
     clientSocket = socket(AF_INET, SOCK_STREAM) #creates client side TCP socket
@@ -25,11 +49,13 @@ def main():
     print(addressInfo)
     t = Thread(target=handleServerMessages, args=(clientSocket,))
     t.start()
-    global connectionOpen
-    connectionOpen = True
-    while connectionOpen:
-        sentence = input('Message:') #reads the string from client side user
-        clientSocket.send(sentence.encode()) # this line encodes and sends the string 
+
+    input_t = Thread(target=handleUserInput, args=(clientSocket,))
+    input_t.start()
+
+    t.join()
+    input_t.join()
+    
 
 if __name__ == "__main__":
     main()
